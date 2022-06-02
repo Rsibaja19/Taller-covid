8678b78f6471aafa9b56ec84111dea0c1648d168
Ricardo Sibaja <rkjsc2000@gmail.com>
2022-06-01 21:18:36 -0500

    Tercer commit

--- tallercovid.py
+++ tallercovid.py
@@ -28,3 +28,68 @@ data.loc[data['Edad'] == 'Casa','Edad'] = 36
 data.loc[data['Edad'] == 'Leve','Edad'] = 36
 data.loc[data['Edad'] == 'M','Edad'] = 36
 data.loc[data['Edad'] == 'F','Edad'] = 36
+
+data.mean()
+
+#Número de casos de Contagiados en el País.
+data['Estado'].count()
+
+#Número de Municipios Afectados
+data['Nombre municipio'].nunique()
+
+#Liste los municipios afectados (sin repetirlos)
+data['Nombre municipio'].value_counts()
+
+#Número de personas que se encuentran en atención en casa
+aux = data.loc[(data['Ubicación del caso'] == 'Casa')]
+NumeroDePersonasEnCasa = aux.shape[0]
+print(f'Número de personas que se encuentran en atención en casa: {NumeroDePersonasEnCasa}')
+
+#Número de personas que se encuentran recuperados
+aux = data.loc[(data['Recuperado'] == 'Recuperado')]
+NumeroDePersonasRecuperadas = aux.shape[0]
+print(f'Número de personas que se encuentran recuperados: {NumeroDePersonasRecuperadas}')
+
+#Número de personas que ha fallecido
+aux = data.loc[(data['Estado'] == 'Fallecido')]
+NumeroDePersonasFallecidas = aux.shape[0]
+print(f'Número de personas que ha fallecido: {NumeroDePersonasFallecidas}')
+
+#Ordenar de Mayor a menor por tipo de caso (Importado, en estudio,Relacionado)
+data.sort_values(by=data.loc[(data['Tipo de contagio'] == 'Importado')],ascending=False )
+data.sort_values(by=data.loc[(data['Tipo de contagio'] == 'Relacionado')],ascending=False )
+
+#Número de departamentos afectados
+data['Nombre departamento'].nunique()
+
+#Liste los departamentos afectados(sin repetirlos)
+data['Nombre departamento'].value_counts()
+
+#Ordene de mayor a menor por tipo de atención
+data.sort_values(by='Ubicación del caso',ascending=False )
+
+#Liste de mayor a menor los 10 departamentos con mas casos de contagiados
+data['Nombre departamento'].value_counts().head(10)
+
+#Liste de mayor a menor los 10 departamentos con mas casos de fallecidos
+aux = data[(data['Estado'] == 'Fallecido')].groupby('Nombre departamento').size()
+
+aux.sort_values(ascending=False).head(10)
+
+#Liste de mayor a menor los 10 departamentos con mas casos de recuperados
+aux = data[(data['Recuperado'] == 'Recuperado')].groupby('Nombre departamento').size()
+
+aux.sort_values(ascending=False).head(10)
+
+#Liste de mayor a menor los 10 municipios con mas casos de contagiados
+data['Nombre municipio'].value_counts().head(10)
+
+#Liste de mayor a menor los 10 municipios con mas casos de fallecidos
+aux = data[(data['Estado'] == 'Fallecido')].groupby('Nombre municipio').size()
+
+aux.sort_values(ascending=False).head(10)
+
+#Liste de mayor a menor los 10 municipios con mas casos de recuperados
+aux = data[(data['Recuperado'] == 'Recuperado')].groupby('Nombre municipio').size()
+
+aux.sort_values(ascending=False).head(10)
